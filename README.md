22020fbc6b38bd809073dc84a3c78aac184d8a15
Adrov Igor <nucleartux@gmail.com>
2019-02-13 00:39:56 +0300

    init

---
+++ README.md
@@ -0,0 +1,78 @@
+## Installation
+
+```sh
+# Using Yarn:
+yarn add react-in-angular react react-dom
+
+# Or, using NPM:
+npm install react-in-angular react react-dom --save
+```
+
+## Usage
+
+### 1. Create a React component
+
+```js
+import { Component } from 'react'
+
+class MyComponent extends Component {
+  render() {
+    return <div>
+      <p>FooBar: {this.props.fooBar}</p>
+      <p>Baz: {this.props.baz}</p>
+    </div>
+  }
+}
+```
+
+### 2. Expose it to Angular
+
+```js
+import reactInAngular from 'react-in-angular'
+
+angular
+  .module('myModule', [])
+  .component('myComponent', reactInAngular(MyComponent, ['fooBar', 'baz']))
+```
+
+### 3. Use it in your Angular 1 code
+
+```html
+<my-component
+  foo-bar="3"
+  baz="'baz'"
+></my-component>
+```
+
+## Dependency Injection
+
+It's easy to pass services/constants/etc. to your React component: just pass them in as the 3rd argument, and they will be available in your component's Props. For example:
+
+```js
+import { Component } from 'react'
+import reactInAngular from 'react-in-angular'
+
+class MyComponent extends Component {
+  state = {
+    data: ''
+  }
+  componentDidMount() {
+    this.props.$http.get('/path').then(res =>
+      this.setState({ data: res.data })
+    )
+  }
+  render() {
+    return <div>
+      { this.props.FOO }
+      { this.state.data }
+    </div>
+  }
+}
+
+angular
+  .module('myModule', [])
+  .constant('FOO', 'FOO!')
+  .component('myComponent', reactInAngular(MyComponent, [], ['$http', 'FOO']))
+```
+
+Note: If you have an injection that matches the name of a prop, then the value will be resolved with the injection, not the prop.
---
+++ src/index.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { fromPairs, pick, reduce } from "lodash-es";
+import * as angular from "angular";
+
+export interface ICtrl {}
+
+/**
+ * Wraps a React component into Angular component. Returns a new Angular component.
+ *
+ * Usage: angular.module('some.module').component('newAngularComponent', angularInReact(MyReactComponent))
+ * (the usage is the same as in similar lib https://github.com/coatue-oss/react2angular)
+ */
+export default function angularInReact<Props>(
+  ComponentClass: React.ComponentType<Props>,
+  names: (keyof Props)[] | null = null,
+  serviceProps: string[] = []
+) {
+  class Ctrl implements ICtrl {
+    static $inject = ["$element", "$scope", "$injector"];
+
+    constructor(
+      private $element: any,
+      private $scope: angular.IScope,
+      private $injector: any
+    ) {}
+
+    wrapFn = (prop: any) => {
+      if (typeof prop === "function") {
+        return (...args: any[]) => {
+          const result = prop(...args);
+          this.$scope.$applyAsync();
+          return result;
+        };
+      }
+
+      return prop;
+    };
+
+    $onChanges() {
+      const props = pick(this, names);
+
+      // Wrap passed angular functions into $apply, because those functions
+      // are supposed to be invoked within React
+      // and we need to notify angular
+      const wrappedProps = reduce(
+        props as any,
+        (result, value, key) => {
+          return {
+            ...result,
+            [key]: this.wrapFn(value)
+          };
+        },
+        {}
+      );
+
+      const services = serviceProps.reduce((result, key) => {
+        return {
+          ...result,
+          [key]: this.$injector.get(key)
+        };
+      }, {});
+
+      ReactDOM.render(
+        <ComponentClass {...wrappedProps} {...services as any} />,
+        this.$element[0]
+      );
+    }
+
+    $onDestroy() {
+      ReactDOM.unmountComponentAtNode(this.$element[0]);
+    }
+  }
+
+  return {
+    bindings: fromPairs(names.map(name => [name, "<"])),
+    controller: Ctrl as ICtrl
+  };
+}
